744cf11ea7e42bcb71ac96bed7bff0f737178e27
Linfeng <linfenglee321@gmail.com>
2015-04-07 21:01:58 -0400

    Finish Reading file

---
+++ ReadingFileAndSearchForText.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+$| = 1;
+
+sub main {
+
+	my $file =
+'E:\Working folder\My learning\Programming\Perl Programming\codes\PerlProjects\free_novel.txt';
+
+	#open the file, if it fails to open the file
+	#then terminate the program
+	open( INPUT, $file ) or die("Input file $file not found\n");
+
+	#<INPUT> read one line of the file and return it, and also
+	#move on to next line
+	while ( my $line = <INPUT> ) {
+
+		#=~ /regular expression/
+		#if line match(contains) the reular expression
+		if ( $line =~ /choice/ ) {
+			print $line;
+		}
+
+	}
+
+	close(INPUT);
+}
+
+main();
